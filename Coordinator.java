5e3131011cf5f9b35e852fb839d3d496430851e3
Divya Dhule <divyadhule5@gmail.com>
2025-12-04 10:35:10 +0530

    Add Coordinator entity with fields and methods

---
+++ Coordinator.java
@@ -0,0 +1,25 @@
+package com.eventmgmt.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coordinators")
+public class Coordinator {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long coordId;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public Coordinator() {}
+    // getters & setters
+    public Long getCoordId() { return coordId; }
+    public void setCoordId(Long coordId) { this.coordId = coordId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
