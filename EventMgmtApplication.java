2509807a640e74c041dc34b161ea3819592d7808
Divya Dhule <divyadhule5@gmail.com>
2025-12-04 10:33:43 +0530

    Create EventMgmtApplication.java

---
+++ EventMgmtApplication.java
@@ -0,0 +1,11 @@
+package com.eventmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventMgmtApplication.class, args);
+    }
+}
